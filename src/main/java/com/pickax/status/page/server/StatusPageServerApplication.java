657d3840dc54a18ee2ecc70933984d04e93550d3
rlatmd0829 <rlatmd0829@naver.com>
2023-12-21 12:56:06 +0900

    git init

---
+++ src/main/java/com/pickax/status/page/server/StatusPageServerApplication.java
@@ -0,0 +1,13 @@
+package com.pickax.status.page.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusPageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusPageServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pickax/status/page/server/StatusPageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickax.status.page.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusPageServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
