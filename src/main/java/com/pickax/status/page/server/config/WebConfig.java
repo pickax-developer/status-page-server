131927e9aacfb26a3b0c5a7d8b8fbb07ccae6788
seungwook-VIRNECT <rlatmd0829@naver.com>
2024-01-03 21:18:14 +0900

    refactor : site에 meta_tag id 추가

---
+++ src/main/java/com/pickax/status/page/server/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.pickax.status.page.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOriginPatterns("*")
+			.allowedMethods("*")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(3600);
+
+
+
+	}
+}
