a651d2b4e1c36348de3e418d66f44eaecfeab263
Seungwook <rlatmd0829@naver.com>
2024-01-10 21:24:49 +0900

    feat : component list 조회 api 구현 및 테스트코드 sql 추가 (#8)
    * feat : component list 조회 api 구현 및 테스트코드 sql 추가
    
    * feat : component list url 변경 및 List Response 생성

--- src/main/java/com/pickax/status/page/server/controller/ComponentController.java
+++ src/main/java/com/pickax/status/page/server/controller/ComponentController.java
@@ -1,8 +1,17 @@
 package com.pickax.status.page.server.controller;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pickax.status.page.server.dto.reseponse.ComponentListResponseDto;
+import com.pickax.status.page.server.dto.reseponse.ComponentResponseDto;
+import com.pickax.status.page.server.service.ComponentService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -11,4 +20,12 @@ import lombok.RequiredArgsConstructor;
 @Validated
 @RequestMapping("/sites")
 public class ComponentController {
+	private final ComponentService componentService;
+
+	@GetMapping("/{siteId}/components/active")
+	public ResponseEntity<ComponentListResponseDto> getActiveComponents(@PathVariable Long siteId) {
+		List<ComponentResponseDto> components = componentService.getActiveComponents(siteId);
+		ComponentListResponseDto componentListResponseDto = new ComponentListResponseDto(components);
+		return ResponseEntity.ok(componentListResponseDto);
+	}
 }
--- src/main/java/com/pickax/status/page/server/service/ComponentService.java
+++ src/main/java/com/pickax/status/page/server/service/ComponentService.java
@@ -1,10 +1,24 @@
 package com.pickax.status.page.server.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.pickax.status.page.server.dto.reseponse.ComponentResponseDto;
+import com.pickax.status.page.server.repository.ComponentRepository;
+import com.pickax.status.page.server.repository.SiteRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ComponentService {
+	private final ComponentRepository componentRepository;
+	private final SiteRepository siteRepository;
+
+	public List<ComponentResponseDto> getActiveComponents(Long siteId) {
+		siteRepository.findById(siteId).orElseThrow(EntityNotFoundException::new);
+		return componentRepository.getComponents(siteId, true);
+	}
 }
