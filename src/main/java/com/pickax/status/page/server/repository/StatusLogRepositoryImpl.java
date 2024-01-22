731e5c16f96e47475206a2d1a69e5cba47f0b9f2
kellyihyeon <kellykang13@gmail.com>
2024-01-23 00:15:43 +0900

    feat: ComponentStatusLog Q파일 생성

--- src/main/java/com/pickax/status/page/server/repository/StatusLogRepositoryImpl.java
+++ src/main/java/com/pickax/status/page/server/repository/StatusLogRepositoryImpl.java
@@ -4,7 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-import static com.pickax.status.page.server.domain.model.QStatusLog.*;
+import static com.pickax.status.page.server.domain.model.QComponentStatusLog.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,10 +15,10 @@ public class StatusLogRepositoryImpl implements StatusLogRepositoryCustom {
     @Override
     public Long findLatestComponentRiskLevel(Long componentId) {
         Long riskLevel = jpaQueryFactory
-                .select(statusLog.riskLevel)
-                .from(statusLog)
-                .where(statusLog.componentId.eq(componentId))
-                .orderBy(statusLog.schedulerRunDateTime.desc())
+                .select(componentStatusLog.riskLevel)
+                .from(componentStatusLog)
+                .where(componentStatusLog.componentId.eq(componentId))
+                .orderBy(componentStatusLog.schedulerRunDateTime.desc())
                 .fetchFirst();
 
         if (riskLevel == null) {
