1209056d86da7964fb783ec90ab7bcda8486aa8d
seungwook-VIRNECT <rlatmd0829@naver.com>
2024-01-24 22:25:53 +0900

    feat : customUserDetails 추가

--- src/main/java/com/pickax/status/page/server/domain/model/User.java
+++ src/main/java/com/pickax/status/page/server/domain/model/User.java
@@ -1,14 +1,13 @@
 package com.pickax.status.page.server.domain.model;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @Table(name = "users")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class User {
 
     @Id
