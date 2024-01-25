386b8abb0653ff6226e85c76e7a8bdc9b2d9b9c4
Seungwook <rlatmd0829@naver.com>
2024-01-25 23:05:02 +0900

    fix : cors 설정 수정 (#32)

--- src/main/java/com/pickax/status/page/server/security/config/SecurityConfig.java
+++ src/main/java/com/pickax/status/page/server/security/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.pickax.status.page.server.security.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.pickax.status.page.server.security.jwt.JwtAccessDeniedHandler;
@@ -21,7 +28,6 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-	private final CorsConfigurationSource customCorsConfigurationSource;
 	private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	private final TokenProvider tokenProvider;
@@ -30,20 +36,46 @@ public class SecurityConfig {
 		return new JwtFilter(tokenProvider);
 	}
 
+	@Bean
+	public CorsConfigurationSource corsConfiguration() {
+		return request -> {
+			org.springframework.web.cors.CorsConfiguration config =
+				new org.springframework.web.cors.CorsConfiguration();
+			config.setAllowedHeaders(Collections.singletonList("*"));
+			config.setAllowedMethods(Collections.singletonList("*"));
+			config.setAllowedOriginPatterns(Collections.singletonList("*"));
+			config.setAllowCredentials(true);
+			return config;
+		};
+	}
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-			.cors(cors -> cors.configurationSource(customCorsConfigurationSource))
+			.cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfiguration()))
 			.csrf(AbstractHttpConfigurer::disable)
 			.exceptionHandling(exception -> exception
 				.authenticationEntryPoint(jwtAuthenticationEntryPoint)
 				.accessDeniedHandler(jwtAccessDeniedHandler)
 			)
+
+			//HTTP 기본 인증 비활성화
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable)
+
+			.headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+
+			//세션 관리 상태 없음으로 구성, Spring Security가 세션 생성 or 사용 X
+			.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(
+				SessionCreationPolicy.STATELESS))
+
+			// 허용 설정
 			.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/sites/**", "/components/**", "/health-check/**").permitAll()
 				.anyRequest().authenticated()
 			)
+
+			// 필터 추가
 			.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
