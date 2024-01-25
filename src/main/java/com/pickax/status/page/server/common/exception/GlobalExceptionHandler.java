fbb04e85533441d14002c87e2a6255d08bba16d2
conagreen <verycona@gmail.com>
2024-01-25 19:26:48 +0900

    feat: 에러 로그 슬랙 연동

--- src/main/java/com/pickax/status/page/server/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pickax/status/page/server/common/exception/GlobalExceptionHandler.java
@@ -2,37 +2,53 @@ package com.pickax.status.page.server.common.exception;
 
 import com.pickax.status.page.server.common.message.LocaleMessageConverter;
 import com.pickax.status.page.server.dto.reseponse.common.ErrorResponse;
+import com.pickax.status.page.server.service.slack.SlackService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolationException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
 	private final LocaleMessageConverter localeMessage;
+	private final SlackService slackService;
 
 	@ExceptionHandler(value = {CustomException.class})
 	protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e, HttpServletRequest request) {
+		sendErrorMessageToSlack(e, localeMessage.getMessage(e.getErrorCode().toString()), e.getErrorCode().getHttpStatus());
 		return ErrorResponse.toResponseEntity(e.getErrorCode(), localeMessage.getMessage(e.getErrorCode().toString()), request);
 	}
 
 	@ExceptionHandler(ConstraintViolationException.class)
 	protected ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e, HttpServletRequest request) {
+		sendErrorMessageToSlack(e, localeMessage.getMessage(ErrorCode.INVALID_INPUT_VALUE.toString()), HttpStatus.BAD_REQUEST);
 		return ErrorResponse.toResponseEntity(ErrorCode.INVALID_INPUT_VALUE, localeMessage.getMessage(ErrorCode.INVALID_INPUT_VALUE.toString()), request);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
-		return ErrorResponse.toResponseEntity(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult(), request);
+		List<FieldError> fieldErrors =  e.getBindingResult().getFieldErrors();
+		String message = fieldErrors.get(0) == null ? "" : fieldErrors.get(0).getDefaultMessage();
+		sendErrorMessageToSlack(e, message, HttpStatus.BAD_REQUEST);
+		return ErrorResponse.toResponseEntity(ErrorCode.INVALID_INPUT_VALUE, message, fieldErrors, request);
 	}
 
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<ErrorResponse> handleException(Exception e, HttpServletRequest request) {
+		sendErrorMessageToSlack(e, e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		return ErrorResponse.toResponseEntity(ErrorCode.INTERNAL_SERVER_ERROR, e.getMessage(), request);
+	}
+
+	private void sendErrorMessageToSlack(Exception e, String message, HttpStatus httpStatus) {
+		slackService.sendErrorMessage(e, message, httpStatus);
 	}
 }
---
+++ src/main/java/com/pickax/status/page/server/service/slack/SlackService.java
@@ -0,0 +1,47 @@
+package com.pickax.status.page.server.service.slack;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.model.Attachment;
+import com.slack.api.model.block.LayoutBlock;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Service
+public class SlackService {
+    @Value(value = "${spring.profiles.active}")
+    String profile;
+
+    @Value(value = "${slack.token}")
+    String token;
+
+    @Value(value = "${slack.channel.monitor}")
+    String channel;
+
+    private static final String PROD = "prod";
+    private static final String PROD_ERROR_MESSAGE_TITLE = "🐤 *꽤액!!! 꽤애액!!! 에러 발생!!!*";
+    private static final String CLIENT_5xx_ERROR_COLOR = "#e82415";
+    private static final String CLIENT_4xx_ERROR_COLOR = "#e8bf1a";
+
+    public void sendErrorMessage(Exception exception, String message, HttpStatus httpStatus) {
+        if (PROD.equals(profile)) {
+            try {
+                String color = httpStatus.is4xxClientError() ? CLIENT_4xx_ERROR_COLOR : CLIENT_5xx_ERROR_COLOR;
+                List<LayoutBlock> layoutBlocks = SlackServiceUtils.createErrorMessage(exception, message);
+                List<Attachment> attachments = SlackServiceUtils.createAttachments(color, layoutBlocks);
+                Slack.getInstance().methods(token).chatPostMessage(request ->
+                        request.channel(channel)
+                                .attachments(attachments)
+                                .text(PROD_ERROR_MESSAGE_TITLE));
+            } catch (SlackApiException | IOException e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+    }
+}
