79c4d6a7985a7966a6491426e0fcfe40ce5b29ab
seungwook-VIRNECT <rlatmd0829@naver.com>
2024-01-25 20:36:14 +0900

    feat : customUserDetails 추가

--- src/main/java/com/pickax/status/page/server/security/service/CustomUserDetails.java
+++ src/main/java/com/pickax/status/page/server/security/service/CustomUserDetails.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CustomUserDetails implements UserDetails {
 	private final Long id;
+	// TODO
 	// private final String nickname;
 	// private final String email;
 
