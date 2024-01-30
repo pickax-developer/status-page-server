e3433c1007550339f5c1db27d92c318a8012f5eb
Seungwook <rlatmd0829@naver.com>
2024-01-30 22:11:32 +0900

    refactor : record 적용 (#35)

--- src/main/java/com/pickax/status/page/server/dto/reseponse/site/SiteSecretKeyResponseDto.java
+++ src/main/java/com/pickax/status/page/server/dto/reseponse/site/SiteSecretKeyResponseDto.java
@@ -1,15 +1,8 @@
 package com.pickax.status.page.server.dto.reseponse.site;
 
-import lombok.Getter;
-
-@Getter
-public class SiteSecretKeyResponseDto {
-	private String secretKey;
-
-	public SiteSecretKeyResponseDto(String secretKey) {
-		this.secretKey = secretKey;
-	}
-
+public record SiteSecretKeyResponseDto(
+	String secretKey
+){
 	public static SiteSecretKeyResponseDto from(String secretKey) {
 		return new SiteSecretKeyResponseDto(secretKey);
 	}
