fef336fdd60909586e1572a6eab1f86dd9e971ed
conagreen <verycona@gmail.com>
2024-01-30 21:08:13 +0900

    feat: User 엔티티 완성

--- src/main/java/com/pickax/status/page/server/domain/model/User.java
+++ src/main/java/com/pickax/status/page/server/domain/model/User.java
@@ -1,8 +1,11 @@
 package com.pickax.status.page.server.domain.model;
 
+import com.pickax.status.page.server.domain.enumclass.UserStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -14,4 +17,19 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "status", length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column(name = "sign_up_at", nullable = false)
+    private LocalDateTime signUpDate;
+
+    @Column(name = "withdrawal_at")
+    private LocalDateTime withdrawalDate;
 }
